aed78a07591f0ca63104d1a99e77a301f266bbe5
faith <faithrono132@gmail.com>
2024-08-03 18:14:11 -0700

    connected redis to the client

---
+++ utils/redis.js
@@ -0,0 +1,41 @@
+// utils/redis.js
+
+import redis from 'redis';
+import { promisify } from 'util';
+
+class RedisClient {
+  constructor() {
+    this.client = redis.createClient();
+    
+    this.client.on('error', (err) => {
+      console.error('Redis client not connected to the server:', err);
+    });
+
+    this.client.on('connect', () => {
+      console.log('Redis client connected to the server');
+    });
+
+    this.getAsync = promisify(this.client.get).bind(this.client);
+    this.setAsync = promisify(this.client.set).bind(this.client);
+    this.delAsync = promisify(this.client.del).bind(this.client);
+  }
+
+  isAlive() {
+    return this.client.connected;
+  }
+
+  async get(key) {
+    return await this.getAsync(key);
+  }
+
+  async set(key, value, duration) {
+    await this.setAsync(key, value, 'EX', duration);
+  }
+
+  async del(key) {
+    await this.delAsync(key);
+  }
+}
+
+const redisClient = new RedisClient();
+export default redisClient;

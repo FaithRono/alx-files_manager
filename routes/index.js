a2e92ea44d2ecfcd91ee1f527301ab892ed03dc5
Owen Gitau <owengitau02@gmail.com>
2024-08-05 08:41:40 +0300

    Authantication of Users

--- routes/index.js
+++ routes/index.js
@@ -3,18 +3,18 @@ const express = require('express');
 const router = express.Router();
 
 const AppController = require('../controllers/AppController');
-//const AuthController = require('../controllers/AuthController');
-//const UsersController = require('../controllers/UsersController');
+const AuthController = require('../controllers/AuthController');
+const UsersController = require('../controllers/UsersController');
 //const FilesController = require('../controllers/FilesController');
 
 
 router.get('/status', AppController.getStatus);
 router.get('/stats', AppController.getStats);
 
-//router.post('/users', UsersController.postNew);
-//router.get('/connect', AuthController.getConnect);
-//router.get('/disconnect', AuthController.getDisconnect);
-//router.get('/users/me', UsersController.getMe);
+router.post('/users', UsersController.postNew);
+router.get('/connect', AuthController.getConnect);
+router.get('/disconnect', AuthController.getDisconnect);
+router.get('/users/me', UsersController.getMe);
 
 //router.post('/files', FilesController.postUpload);
 //router.get('/files/:id', FilesController.getShow);
